f0ab815f26939648ee4094d32d168f7576c66939
JayWebDevCom <jaiye.s@gmail.com>
2017-07-20 23:17:58 +0100

    add Calculator and tested add method

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.stream.DoubleStream;
+
+public class Calculator {
+    public double sum(ArrayList<Double> purchases, ArrayList<Double> savings) {
+        return purchases.stream().reduce(0.0, Double::sum) + savings.stream().reduce(0.0, Double::sum);
+    }
+}
---
+++ src/CalculatorTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    ArrayList<Double> purchases;
+    ArrayList<Double> savings;
+    Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        purchases = new ArrayList<>(Arrays.asList(2.25, 2.50, 2.75));
+        savings = new ArrayList<>(Arrays.asList(-2.00));
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void addSumsTwoListsOfDoubles() {
+        assertEquals(5.50, calculator.sum(purchases, savings), 0.0);
+    }
+
+
+}
