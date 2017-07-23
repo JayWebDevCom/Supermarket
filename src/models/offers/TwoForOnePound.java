32f9b4bba033d5ce7a1627583d28b2f2a63f5a07
JayWebDevCom <jaiye.s@gmail.com>
2017-07-23 19:41:37 +0100

    add passing tests for 2 for £1

--- src/models/offers/TwoForOnePound.java
+++ src/models/offers/TwoForOnePound.java
@@ -2,8 +2,14 @@ package models.offers;
 
 import models.Basket;
 import models.Product;
+import models.Saving;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TwoForOnePound extends Offer {
+
+    private static final int REQUIRED_NUMBER = 2;
 
     public TwoForOnePound(String aName, Product... products) {
         super(aName, products);
@@ -12,5 +18,25 @@ public class TwoForOnePound extends Offer {
     @Override
     public void applyToBasket(Basket basket){
 
+        ArrayList<Product> doneItems = new ArrayList<Product>();
+
+        for (Product product : basket.getItems()) {
+
+            if (!doneItems.contains(product) && includedProducts.contains(product)) {
+
+                int countOfItemInBasket = Collections.frequency(basket.getItems(), product);
+                double numSaving = Math.floor(countOfItemInBasket / REQUIRED_NUMBER);
+                int numSavings = (int) numSaving;
+                double saving = (REQUIRED_NUMBER * product.getPrice()) - 1.00;
+
+                for (int i = 0; i < numSavings; i++) {
+                    basket.addSaving(new Saving(product.getName(), saving, product.getName() + ' ' + getName()));
+                }
+
+            }
+
+            doneItems.add(product);
+
+        }
     }
 }
--- src/tests/TwoForOnePoundTest.java
+++ src/tests/TwoForOnePoundTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import models.Basket;
 import models.Product;
 import models.offers.TwoForOnePound;
 import org.junit.Before;
@@ -11,16 +12,45 @@ public class TwoForOnePoundTest {
 
     private Product coke;
     private TwoForOnePound tfop;
+    Basket basket;
 
     @Before
     public void setUp() throws Exception {
         coke = new Product("Coke", 0.70);
         tfop = new TwoForOnePound("Two for £1", coke);
+        basket = new Basket(coke, coke, coke, coke);
     }
 
     @Test
     public void acceptsCoke(){
         assertEquals(1, tfop.getIncludedProducts().size());
+    }
+
+    @Test
+    public void zeroSavingsForOneCoke(){
+        basket = new Basket(coke);
+        tfop.applyToBasket(basket);
+        assertEquals(0, basket.getSavingsItems().size());
+    }
+
+    @Test
+    public void oneSavingForTwoCokes(){
+        basket = new Basket(coke, coke);
+        tfop.applyToBasket(basket);
+        assertEquals(1, basket.getSavingsItems().size());
+    }
+
+    @Test
+    public void twoSavingsForThreeCokes(){
+        tfop.applyToBasket(basket);
+        assertEquals(2, basket.getSavingsItems().size());
+    }
+
+    @Test
+    public void twoSavingsForFourCokes(){
+        basket.add(coke);
+        tfop.applyToBasket(basket);
+        assertEquals(2, basket.getSavingsItems().size());
     }
 
 }
